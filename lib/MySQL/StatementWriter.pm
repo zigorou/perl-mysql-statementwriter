a1635525e78b53680dd53824b14f1b7400611bc7
Toru Yamaguchi <zigorou@cpan.org>
2013-02-23 03:31:47 +0900

    initial import

---
+++ lib/MySQL/StatementWriter.pm
@@ -0,0 +1,41 @@
+package MySQL::StatementWriter;
+use strict;
+use warnings;
+use 5.008005;
+our $VERSION = '0.0.1';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+MySQL::StatementWriter - ...
+
+=head1 SYNOPSIS
+
+  use MySQL::StatementWriter;
+
+=head1 DESCRIPTION
+
+MySQL::StatementWriter is
+
+B<THIS IS A DEVELOPMENT RELEASE. API MAY CHANGE WITHOUT NOTICE>.
+
+=head1 AUTHOR
+
+Toru Yamaguchi E<lt>zigorou@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Toru Yamaguchi
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
---
+++ t/00_compile.t
@@ -0,0 +1,8 @@
+use strict;
+use Test::More;
+
+use_ok $_ for qw(
+    MySQL::StatementWriter
+);
+
+done_testing;
